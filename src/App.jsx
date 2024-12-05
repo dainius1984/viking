0cd36466e26ba0cd902be3f66e2135bca558cd33
dainius1984 <marcinarturchmielnicki@gmail.com>
2024-12-05 07:12:25 -0800

    feat: new1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import HomePage from './pages/HomePage';
 import CategoryPage from './components/Pages/CategoryPage';
-import Cart from './components/Pages/Cart'; // 
+import Cart from './components/Pages/Cart';
 import Wishlist from './components/Pages/Wishlist';
 import Blog from './components/Pages/Blog';
-
+import Article from './components/Pages/Article'; // Add this import
 
 const App = () => {
   return (
@@ -17,8 +18,9 @@ const App = () => {
           <Route path="/category" element={<CategoryPage />} />
           <Route path="/category/:categorySlug" element={<CategoryPage />} />
           <Route path="/koszyk" element={<Cart />} />
-          <Route path="/wishlist" element={<Wishlist />} /> {/* Add this route */}
-          <Route path="/blog" element={<Blog />} /> 
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/article/:id" element={<Article />} /> {/* Add this route */}
         </Routes>
       </Router>
     </CartProvider>
---
+++ src/Data/blog-data.js
@@ -0,0 +1,45 @@
+// Data/blog-data.js
+export const blogHeaderData = {
+    title: "Blog",
+    subtitle: "Zdrowie i Witaminy",
+    heroImage: "img/blog.jpg",
+  };
+  
+  export const blogPosts = [
+    {
+      id: 1,
+      title: "Kiedy najlepiej brać witaminy – Rano czy wieczorem?",
+      image: "/img/blog/1.jpg",
+      date: "2024-01-15",
+      category: "Zdrowie",
+      slug: "kiedy-najlepiej-brac-witaminy"
+    },
+    {
+      id: 2,
+      title: " Jak dieta, sen i aktywność fizyczna wpływają na nasze samopoczucie?",
+      image: "/img/blog/2.jpg",
+      date: "2024-01-10",
+      category: "Zdrowie"
+    },
+    {
+      id: 3,
+      title: "Stres i zdrowie: Jak radzić sobie z napięciem w codziennym życiu?",
+      image: "/img/blog/3.jpg",
+      date: "2024-01-05",
+      category: "Zdrowie"
+    },
+    {
+        id: 4,
+        title: "Odporność organizmu w praktyce: Jak wzmocnić układ immunologiczny naturalnymi metodami?",
+        image: "/img/blog/4.jpg",
+        date: "2024-01-05",
+        category: "Zdrowie"
+      }
+  ];
+  
+  export const recentPosts = [
+    "Jak witaminami wzmocnić odporność w czasie zimy?",
+    "Naturalne źródła witaminy D3",
+    "Kwasy Omega-3 – Działanie, Źródła, Dawkowanie",
+    "Witaminy dla kobiet w ciąży"
+  ];
